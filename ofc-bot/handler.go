06bab140612648696b51741d849e6da1fc4ae2d9
Alex Ellis <alexellis2@gmail.com>
2019-05-14 09:46:05 +0100

    Break out method for commands
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- ofc-bot/handler.go
+++ ofc-bot/handler.go
@@ -2,6 +2,7 @@ package function
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -11,6 +12,117 @@ import (
 )
 
 const owner = "com.openfaas.cloud.git-owner"
+
+type function struct {
+	Name            string            `json:"name"`
+	Image           string            `json:"image"`
+	InvocationCount float64           `json:"invocationCount"`
+	Replicas        uint64            `json:"replicas"`
+	Labels          map[string]string `json:"labels"`
+	Annotations     map[string]string `json:"annotations"`
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	var input []byte
+
+	if r.Body != nil {
+		defer r.Body.Close()
+
+		body, _ := ioutil.ReadAll(r.Body)
+
+		input = body
+	}
+
+	token, err := sdk.ReadSecret("token")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	var query url.Values
+	if len(input) > 0 {
+		q, err := url.ParseQuery(string(input))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		query = q
+	}
+
+	if token != query.Get("token") {
+		http.Error(w, fmt.Sprintf("Token: %s, invalid", query.Get("token")), http.StatusUnauthorized)
+	}
+	headerWritten := processCommand(w, r, query)
+
+	if !headerWritten {
+		http.Error(w, "Nothing to do", http.StatusBadRequest)
+	}
+}
+
+func processCommand(w http.ResponseWriter, r *http.Request, query url.Values) bool {
+	if cmd := query.Get("command"); len(cmd) > 0 {
+		if cmd == "/functions" {
+
+			res, err := doFunctionsQuery()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return true
+			}
+
+			functions, err := readFunctions(res)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return true
+			}
+
+			var username string
+			if text := query.Get("text"); len(cmd) > 0 {
+				username = text
+			}
+
+			out := ""
+			list := makeFunctions(&functions, username)
+			if len(list) > 0 {
+				for _, k := range list {
+					out = out + k + "\n"
+				}
+			} else {
+				out = "No functions found"
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(out))
+			return true
+		}
+		if cmd == "/users" {
+
+			res, err := doFunctionsQuery()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return true
+			}
+
+			functions, err := readFunctions(res)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return true
+			}
+
+			out := ""
+			owners := makeOwners(&functions)
+			if len(owners) > 0 {
+				for k := range owners {
+					out = out + k + "\n"
+				}
+			} else {
+				out = "No users found"
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(out))
+			return true
+		}
+	}
+	return false
+}
 
 func doFunctionsQuery() (*http.Response, error) {
 	getPath := os.Getenv("gateway_host") + "/system/functions"
@@ -26,85 +138,19 @@ func doFunctionsQuery() (*http.Response, error) {
 	return res, err
 }
 
-func Handle(w http.ResponseWriter, r *http.Request) {
-	var input []byte
+func readFunctions(res *http.Response) ([]function, error) {
+	functions := []function{}
 
-	if r.Body != nil {
-		defer r.Body.Close()
-
-		body, _ := ioutil.ReadAll(r.Body)
-
-		input = body
-	}
-
-	if len(input) > 0 {
-		q, err := url.ParseQuery(string(input))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if cmd := q.Get("command"); len(cmd) > 0 {
-			if cmd == "/functions" {
-
-				res, err := doFunctionsQuery()
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				}
-
-				functions, err := readFunctions(res)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				}
-
-				var username string
-				if text := q.Get("text"); len(cmd) > 0 {
-					username = text
-				}
-
-				list := makeFunctions(&functions, username)
-				if len(list) > 0 {
-					for _, k := range list {
-						out = out + k + "\n"
-					}
-				} else {
-					out = "No functions found"
-				}
-
-				out += "\n" + string(input)
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(out))
-				return
-			}
-			if cmd == "/users" {
-
-				res, err := doFunctionsQuery()
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				}
-
-				functions, err := readFunctions(res)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				}
-
-				out := ""
-				owners := makeOwners(&functions)
-				if len(owners) > 0 {
-					for k := range owners {
-						out = out + k + "\n"
-					}
-				} else {
-					out = "No users found"
-				}
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(out))
-				return
-			}
+	if res.Body != nil {
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		marshalErr := json.Unmarshal(body, &functions)
+		if marshalErr != nil {
+			return nil, marshalErr
 		}
 	}
 
-	http.Error(w, "Nothing to do", http.StatusBadRequest)
+	return functions, nil
 }
 
 func makeFunctions(functions *[]function, username string) []string {
@@ -139,28 +185,4 @@ func makeOwners(functions *[]function) map[string]int {
 		}
 	}
 	return owners
-}
-
-func readFunctions(res *http.Response) ([]function, error) {
-	functions := []function{}
-
-	if res.Body != nil {
-		defer res.Body.Close()
-		body, _ := ioutil.ReadAll(res.Body)
-		marshalErr := json.Unmarshal(body, &functions)
-		if marshalErr != nil {
-			return nil, marshalErr
-		}
-	}
-
-	return functions, nil
-}
-
-type function struct {
-	Name            string            `json:"name"`
-	Image           string            `json:"image"`
-	InvocationCount float64           `json:"invocationCount"`
-	Replicas        uint64            `json:"replicas"`
-	Labels          map[string]string `json:"labels"`
-	Annotations     map[string]string `json:"annotations"`
 }
